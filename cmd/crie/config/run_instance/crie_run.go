e6902a2cf18d45cf81194d5ce8e4b0456b189637
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-08-26 18:01:20 +0200

    Add additional run configurations

--- cmd/crie/config/run_instance/crie_run.go
+++ cmd/crie/config/run_instance/crie_run.go
@@ -1,12 +1,13 @@
 package run_instance
 
 import (
+	"regexp"
+	"strings"
+
 	"github.com/tyhal/crie/cmd/crie/config/language"
 	"github.com/tyhal/crie/cmd/crie/config/project"
 	"github.com/tyhal/crie/pkg/crie"
 	"github.com/tyhal/crie/pkg/crie/linter"
-	"regexp"
-	"strings"
 )
 
 // TODO stop using globals for dumping config somewhere
@@ -16,7 +17,7 @@ var Crie crie.RunConfiguration // 3. final config
 func SaveConfiguration(proj *project.ConfigProject, langs *language.ConfigLanguages) {
 	Crie.Ignore = regexp.MustCompile(strings.Join(proj.Ignore, "|"))
 
-	Crie.Languages = make(map[string]*linter.Language)
+	Crie.Languages = make(map[string]*linter.Language, len(langs.Languages))
 	for langName, lang := range langs.Languages {
 		Crie.Languages[langName] = lang.ToCrieLanguage()
 	}
