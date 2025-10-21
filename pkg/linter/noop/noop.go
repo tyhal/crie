5995198a74307a7803a78e473867c784553ba1c3
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-10-21 23:44:14 +0100

    Add tests checking WillRun and Run errors

--- pkg/linter/noop/noop.go
+++ pkg/linter/noop/noop.go
@@ -9,7 +9,18 @@ import (
 
 // LintNoop performs no operations, as a template implementation of cries' linter.Linter
 type LintNoop struct {
-	Type string `json:"type" yaml:"type" jsonschema:"enum=noop" jsonschema_description:"a linter type to do nothing"`
+	Type       string `json:"type" yaml:"type" jsonschema:"enum=noop" jsonschema_description:"a linter type to do nothing"`
+	willRunErr error
+	runErr     error
+}
+
+// WithErr creates a new LintNoop with the given errors
+func WithErr(willRunErr error, runErr error) *LintNoop {
+	return &LintNoop{
+		Type:       "noop",
+		runErr:     runErr,
+		willRunErr: willRunErr,
+	}
 }
 
 var _ linter.Linter = (*LintNoop)(nil)
@@ -19,9 +30,9 @@ func (l *LintNoop) Name() string {
 	return "noop"
 }
 
-// WillRun do nothing as there are no external deps
+// WillRun just returns the configured error
 func (l *LintNoop) WillRun() (err error) {
-	return nil
+	return l.willRunErr
 }
 
 // Cleanup removes any additional resources created in the process
@@ -34,7 +45,7 @@ func (l *LintNoop) MaxConcurrency() int {
 	return math.MaxInt32
 }
 
-// Run the linter
+// Run will just return the configured error as a report
 func (l *LintNoop) Run(filepath string, rep chan linter.Report) {
-	rep <- linter.Report{File: filepath, Err: nil, StdOut: nil, StdErr: nil}
+	rep <- linter.Report{File: filepath, Err: l.runErr, StdOut: nil, StdErr: nil}
 }
