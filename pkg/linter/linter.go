de058f55e3008936fddfeccb317795c3ad76bc31
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-10-24 23:01:18 +0100

    Fix recursion bug

--- pkg/linter/linter.go
+++ pkg/linter/linter.go
@@ -5,23 +5,6 @@ import (
 	"sync"
 )
 
-// FailedResultError means a linter didn't error but returned a failed result
-type FailedResultError struct {
-	err error
-}
-
-func (e *FailedResultError) Error() string {
-	return e.Error()
-}
-
-// Result conditionally wraps an error with a FailedResultError or otherwise passes through nil, it should be used when a linter didn't error but returned a failed result
-func Result(err error) error {
-	if err == nil {
-		return nil
-	}
-	return &FailedResultError{err: err}
-}
-
 // Report is used to state what issues a given file has
 type Report struct {
 	File   string
---
+++ pkg/linter/result_test.go
@@ -0,0 +1,34 @@
+package linter
+
+import (
+    "errors"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestFailedResultError_Error_ReturnsUnderlying(t *testing.T) {
+    under := errors.New("linter reported failure")
+    fre := &FailedResultError{err: under}
+
+    // Ensure Error() returns the underlying message (and does not recurse)
+    assert.Equal(t, under.Error(), fre.Error())
+}
+
+func TestResult_NilPassThrough(t *testing.T) {
+    assert.Nil(t, Result(nil))
+}
+
+func TestResult_WrapsNonNilError(t *testing.T) {
+    under := errors.New("failed rules")
+    err := Result(under)
+
+    // Not nil and of the correct type
+    if assert.NotNil(t, err) {
+        var fre *FailedResultError
+        if assert.ErrorAs(t, err, &fre) {
+            // Error string should match underlying error
+            assert.Equal(t, under.Error(), fre.Error())
+        }
+    }
+}
